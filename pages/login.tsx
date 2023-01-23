ab6039ce3fb4469b10a64a77c0d54d683f51d693
Bastin Jafari <bastinjafari@gmail.com>
2023-01-23 18:31:08 +0100

    feat: newest changes

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,93 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { NextPage } from 'next'
 
-const LoginPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>Login</h1>
-    <p>This is the Login page</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
-  </Layout>
-)
+// The login page
 
-export default LoginPage
+const Login: NextPage = () => {
+  return (
+    <Layout title="Login">
+      <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm leading-5 text-gray-600 max-w">
+            Or
+            <Link href="/register">
+              <a className="font-medium text-blue-600 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                {' '}
+                create a new account
+              </a>
+            </Link>
+          </p>
+        </div>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow-md rounded-lg">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-5 text-gray-700">
+                  Email address
+                </label>
+                <div className="mt-1 rounded-md shadow-sm">
+                  <input
+                    id="email"
+                    type="email"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="password" className="block text-sm font-medium leading-5 text-gray-700">
+                  Password
+                </label>
+                <div className="mt-1 rounded-md shadow-sm">
+                  <input
+                    id="password"
+                    type="password"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                </div>
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember_me"
+                    type="checkbox"
+                    className="form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out"
+                  />
+                  <label htmlFor="remember_me" className="ml-2 block text-sm leading-5 text-gray-900">
+                    Remember me
+                  </label>
+                </div>
+                <div className="text-sm leading-5">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-600 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+              <div>
+                <span className="block w-full rounded-md shadow-sm">
+                  <button
+
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
+                  >
+                    Sign in
+                  </button>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Login
