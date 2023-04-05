2ee678a79329bbe8ab85be5d21347e3291e4a98f
Bastin <BastinJafari@gmail.com>
2023-04-05 19:56:42 +0200

    feat: basic updateschema and tidy up

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,25 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient, User } from '@prisma/client';
 
 
 // Register a new user in the database and return the user object if successful or an error message if not successful
 
-const prisma = new PrismaClient();
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     console.log("test")
-    const { name, email, password } = req.body;
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email,
-        password,
-      },
-    });
-    res.status(200).json({ user });
-  } catch (error) {
-    console.log(error)
+
+} catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
--- scripts/updateSchema.ts
+++ scripts/updateSchema.ts
@@ -1,8 +1,25 @@
-import { DgraphClient, DgraphClientStub } from "dgraph-js-http";
+import { DgraphClient, clientStubFromCloudEndpoint, Operation } from "dgraph-js";
+import { credentials } from "@grpc/grpc-js";
 import fs from 'fs'
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const clientStub = new DgraphClientStub("http://localhost:8080")
+
+
+const dbURl = process.env.DATABASE_URL || "http://localhost:8080";
+const apiKey = process.env.DGRAPH_CLOUD_API_KEY;
+const clientStub = clientStubFromCloudEndpoint(dbURl, apiKey)
 const dgraphClient = new DgraphClient(clientStub);
 
-const schema = fs.readFileSync("../schema/schema.graphql", "utf8");
-dgraphClient.setCloudApiKey("<api-key>");
+async function updateSchema() {
+    // const schemaPath = path.join(__dirname, '../schema/schema.graphql');
+    // const schema = fs.readFileSync(schemaPath, 'utf8')
+const schema = "name: string @index(exact) .";
+const op = new Operation();
+op.setSchema(schema);
+await dgraphClient.alter(op);
+
+
+}
+updateSchema()
