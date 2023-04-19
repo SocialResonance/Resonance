94d96a6d247fd214914854b3f460eb2c26b0ded6
Bastin <BastinJafari@gmail.com>
2023-04-20 00:01:16 +0200

    style: format test

--- tests/api/register.test.tsx
+++ tests/api/register.test.tsx
@@ -1,25 +1,25 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient, User } from '@prisma/client';
-import register from '../../pages/api/register';
+import { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient, User } from '@prisma/client'
+import register from '../../pages/api/register'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 jest.mock('@prisma/client', () => {
   const userMock = {
     create: jest.fn(),
-  };
+  }
 
   return {
     PrismaClient: jest.fn().mockImplementation(() => ({
       user: userMock,
     })),
-  };
-});
+  }
+})
 
 describe('register API handler', () => {
   afterEach(() => {
-    jest.clearAllMocks();
-  });
+    jest.clearAllMocks()
+  })
 
   it('should register a new user and return the user object', async () => {
     const req = {
@@ -28,12 +28,12 @@ describe('register API handler', () => {
         email: 'john@example.com',
         password: 'password123',
       },
-    } as NextApiRequest;
+    } as NextApiRequest
 
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
-    } as unknown as NextApiResponse;
+    } as unknown as NextApiResponse
 
     const expectedUser: User = {
       id: 1,
@@ -42,16 +42,15 @@ describe('register API handler', () => {
       password: 'password123',
       createdAt: new Date(),
       updatedAt: new Date(),
-  
-    };
+    }
 
-    prisma.user.create = jest.fn().mockResolvedValue(expectedUser);
+    prisma.user.create = jest.fn().mockResolvedValue(expectedUser)
 
-    await register(req, res);
+    await register(req, res)
 
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith({ user: expectedUser });
-  });
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith({ user: expectedUser })
+  })
 
   it('should return a 500 error if there is an error during user registration', async () => {
     const req = {
@@ -60,20 +59,20 @@ describe('register API handler', () => {
         email: 'john@example.com',
         password: 'password123',
       },
-    } as NextApiRequest;
+    } as NextApiRequest
 
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
-    } as unknown as NextApiResponse;
+    } as unknown as NextApiResponse
 
-    const errorMessage = 'An error occurred during registration';
+    const errorMessage = 'An error occurred during registration'
 
-    prisma.user.create = jest.fn().mockRejectedValue(new Error(errorMessage));
+    prisma.user.create = jest.fn().mockRejectedValue(new Error(errorMessage))
 
-    await register(req, res);
+    await register(req, res)
 
-    expect(res.status).toHaveBeenCalledWith(500);
-    expect(res.json).toHaveBeenCalledWith({ error: errorMessage });
-  });
-});
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.json).toHaveBeenCalledWith({ error: errorMessage })
+  })
+})
